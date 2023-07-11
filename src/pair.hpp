742c2f307f750f6e2bb2ae92b162eae553ca642e
fengjji <fengjji@163.com>
2023-07-11 23:56:35 +0800

    对pair注释的修改

--- src/pair.hpp
+++ src/pair.hpp
@@ -7,13 +7,17 @@ template <typename T1, typename T2> struct pair {
   using first_type = T1;
   using second_type = T2;
 
-  // 我们知道类中若有引用变量
+  // 我们知道类中若有引用变量 （有时可能带有const修饰符）
   // 需要在构造函数的时候初始化的
-  // 所以若参数是 & 就不对了
+  // 故视情况而定时候是否需要在<>里面写&
+  // 结合目前对C++组件的使用情况看，貌似很少在<>里写&
+  // 更多的就是单纯写一下类型名 （即class后面那窜字符仅此而已）
+
   first_type first;
   second_type second;
 
   // 这个写法参考了MSVC2019
+  // boost也这种写法
   pair() : first(), second() {}
 
   pair(const first_type &a, const second_type &b) : first(a), second(b) {}
@@ -22,6 +26,7 @@ template <typename T1, typename T2> struct pair {
   pair(const pair &arg) : first(arg.first), second(arg.second) {}
 
   // 右值引用版
+  //
   // pair(pair&& arg):fisrt(std::move(arg.first)),second(std::move(arg,second))
   // noexcept {}
 
